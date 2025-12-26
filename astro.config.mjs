a7d230569df90b652c1d4059159f6898786c652f
bvislao <bvislao95@gmail.com>
2025-12-26 08:57:18 -0500

    refactor: components and run with pnpm

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import tailwindcss from '@tailwindcss/vite';
+import vercel from '@astrojs/vercel';
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx()],
+  integrations: [mdx(),],
 
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+  adapter: vercel(),
 });
---
+++ src/data/project.ts
@@ -0,0 +1,8 @@
+export const projects = [
+  {
+    title: "Mi proyecto 1",
+    description: "Descripción breve orientada a impacto.",
+    stack: ["Astro", "Tailwind", "Supabase"],
+    href: "#",
+  },
+];
